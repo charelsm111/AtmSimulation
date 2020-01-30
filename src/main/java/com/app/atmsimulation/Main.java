e3d2acda28fcf2d029d3b708daf785df9d10e9af
Charel Samuel Matulessy <Charel_SM111@mitrais.com>
2020-01-30 09:47:41 +0700

    change main method to spring style

--- src/main/java/com/app/atmsimulation/Main.java
+++ src/main/java/com/app/atmsimulation/Main.java
@@ -1,8 +1,16 @@
 package com.app.atmsimulation;
 
-public class Main {
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+
+public class Main implements CommandLineRunner {
 
     public static void main(String[] args) {
+        SpringApplication.run(Main.class, args);
+    }
+
+    @Override
+    public void run(String... args) {
         Screen screen = new WelcomeScreen();
         screen.show();
     }
