788a23eb92220f47d32afd755677c63e61f86aea
Charel Samuel Matulessy <Charel_SM111@mitrais.com>
2020-02-25 14:59:22 +0700

    add SpringBootApplication annotation

--- src/main/java/com/app/atmsimulation/Main.java
+++ src/main/java/com/app/atmsimulation/Main.java
@@ -2,7 +2,9 @@ package com.app.atmsimulation;
 
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@SpringBootApplication
 public class Main implements CommandLineRunner {
 
     public static void main(String[] args) {
