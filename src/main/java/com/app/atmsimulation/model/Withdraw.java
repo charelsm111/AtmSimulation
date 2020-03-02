ba3b243451d1b506678ee7345d3fe8eaa5797bd6
Charel Samuel Matulessy <Charel_SM111@mitrais.com>
2020-03-02 15:06:00 +0700

    Add constructor

--- src/main/java/com/app/atmsimulation/model/Withdraw.java
+++ src/main/java/com/app/atmsimulation/model/Withdraw.java
@@ -2,9 +2,16 @@ package com.app.atmsimulation.model;
 
 import javax.persistence.DiscriminatorValue;
 import javax.persistence.Entity;
+import java.util.Date;
 
 @Entity
 @DiscriminatorValue("withdraw")
 public class Withdraw extends Transaction {
 
+    public Withdraw() {}
+    
+    public Withdraw(int amount, Account account) {
+        this.setAmount(amount);
+        this.setAccount(account);
+    }
 }
