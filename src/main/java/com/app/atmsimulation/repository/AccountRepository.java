52cbe7037e3babf4897edd147013bf24cc685db4
Charel Samuel Matulessy <Charel_SM111@mitrais.com>
2020-03-02 13:07:46 +0700

    add findAll function

--- src/main/java/com/app/atmsimulation/repository/AccountRepository.java
+++ src/main/java/com/app/atmsimulation/repository/AccountRepository.java
@@ -4,10 +4,14 @@ import com.app.atmsimulation.model.Account;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface AccountRepository extends JpaRepository<Account, Long> {
 
     Account findByAccountNumberAndPin(String accountNumber, String pin);
 
     Account findByAccountNumber(String accountNumber);
+
+    List<Account> findAll();
 }
