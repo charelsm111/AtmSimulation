437687469cf8221d42ba8fe8d0a7ac16761ee574
Charel Samuel Matulessy <Charel_SM111@mitrais.com>
2020-03-02 15:07:59 +0700

    Add save method

--- src/main/java/com/app/atmsimulation/service/AccountService.java
+++ src/main/java/com/app/atmsimulation/service/AccountService.java
@@ -5,4 +5,6 @@ import com.app.atmsimulation.model.Account;
 public interface AccountService {
 
     public Account login(String accountNumber, String pin);
+
+    public Account save(Account account);
 }
