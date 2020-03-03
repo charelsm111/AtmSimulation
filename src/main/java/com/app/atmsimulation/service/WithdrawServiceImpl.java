004df1aa9c1b3fef42e2a220e058274bd851196a
Charel Samuel Matulessy <Charel_SM111@mitrais.com>
2020-03-03 13:42:11 +0700

    reduce account balance when withdraw

--- src/main/java/com/app/atmsimulation/service/WithdrawServiceImpl.java
+++ src/main/java/com/app/atmsimulation/service/WithdrawServiceImpl.java
@@ -1,6 +1,8 @@
 package com.app.atmsimulation.service;
 
+import com.app.atmsimulation.model.Account;
 import com.app.atmsimulation.model.Withdraw;
+import com.app.atmsimulation.repository.AccountRepository;
 import com.app.atmsimulation.repository.WithdrawRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -11,8 +13,16 @@ public class WithdrawServiceImpl implements WithdrawService {
     @Autowired
     private WithdrawRepository withdrawRepository;
 
+    @Autowired
+    private AccountRepository accountRepository;
+
     @Override
     public Withdraw save(Withdraw withdraw) {
+        Account account = withdraw.getAccount();
+        Integer balance = account.getBalance() - withdraw.getAmount();
+
+        account.setBalance(balance);
+        accountRepository.save(account);
 
         return withdrawRepository.save(withdraw);
     }
