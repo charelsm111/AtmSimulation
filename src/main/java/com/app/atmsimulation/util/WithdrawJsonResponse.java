7d796a3a0197485e7d0380f091fd1c31c08a4ea9
Charel Samuel Matulessy <Charel_SM111@mitrais.com>
2020-03-03 13:46:43 +0700

    add WithdrawJsonResponse

---
+++ src/main/java/com/app/atmsimulation/util/WithdrawJsonResponse.java
@@ -0,0 +1,37 @@
+package com.app.atmsimulation.util;
+
+import com.app.atmsimulation.model.Withdraw;
+
+import java.util.List;
+import java.util.Map;
+
+public class WithdrawJsonResponse {
+
+    private Withdraw withdraw;
+    private boolean isValid;
+    private List<String> errorMessages;
+
+    public void setWithdraw(Withdraw withdraw) {
+        this.withdraw = withdraw;
+    }
+
+    public Withdraw getWithdraw() {
+        return withdraw;
+    }
+
+    public void setValid(boolean isValid) {
+        this.isValid = isValid;
+    }
+
+    public boolean isValid() {
+        return isValid;
+    }
+
+    public void setErrorMessages(List<String> errorMessages) {
+        this.errorMessages = errorMessages;
+    }
+
+    public List<String> getErrorMessages() {
+        return errorMessages;
+    }
+}
