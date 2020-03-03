24d42c3839b7fdc545080fd5ace6b43a8465a17a
Charel Samuel Matulessy <Charel_SM111@mitrais.com>
2020-03-03 15:26:50 +0700

    add setter and getter

--- src/main/java/com/app/atmsimulation/model/Transfer.java
+++ src/main/java/com/app/atmsimulation/model/Transfer.java
@@ -10,4 +10,20 @@ public class Transfer extends Transaction {
 
     @Column
     private String destinationAccountNumber;
+
+    public Transfer() {}
+
+    public Transfer(int amount, Account account, String destinationAccountNumber) {
+        this.setAmount(amount);
+        this.setAccount(account);
+        this.destinationAccountNumber = destinationAccountNumber;
+    }
+
+    public void setDestinationAccountNumber(String destinationAccountNumber) {
+        this.destinationAccountNumber = destinationAccountNumber;
+    }
+
+    public String getDestinationAccountNumber() {
+        return destinationAccountNumber;
+    }
 }
