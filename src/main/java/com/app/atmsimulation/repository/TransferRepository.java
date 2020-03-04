6bb578ca3a0898e1ce05ed2b7044bfd501cfbe92
Charel Samuel Matulessy <Charel_SM111@mitrais.com>
2020-03-04 10:11:30 +0700

    add Repository annotation

--- src/main/java/com/app/atmsimulation/repository/TransferRepository.java
+++ src/main/java/com/app/atmsimulation/repository/TransferRepository.java
@@ -2,7 +2,9 @@ package com.app.atmsimulation.repository;
 
 import com.app.atmsimulation.model.Transfer;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface TransferRepository extends CrudRepository<Transfer, Long> {
 
     Transfer save(Transfer transfer);
