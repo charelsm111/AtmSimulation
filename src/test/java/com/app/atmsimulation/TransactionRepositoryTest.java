3a0e48b805c243fc44f1072372a33e9cf655e612
Charel Samuel Matulessy <Charel_SM111@mitrais.com>
2020-03-05 09:08:03 +0700

    add TransactionRepositoryTest

---
+++ src/test/java/com/app/atmsimulation/TransactionRepositoryTest.java
@@ -0,0 +1,76 @@
+package com.app.atmsimulation;
+
+import com.app.atmsimulation.model.Account;
+import com.app.atmsimulation.model.Transaction;
+import com.app.atmsimulation.model.Transfer;
+import com.app.atmsimulation.model.Withdraw;
+import com.app.atmsimulation.repository.AccountRepository;
+import com.app.atmsimulation.repository.TransactionRepository;
+import com.app.atmsimulation.repository.TransferRepository;
+import com.app.atmsimulation.repository.WithdrawRepository;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.List;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.*;
+import static org.mockito.Mockito.when;
+
+@RunWith(SpringRunner.class)
+@DataJpaTest
+public class TransactionRepositoryTest {
+
+    @Autowired
+    private TransactionRepository transactionRepository;
+
+    @Mock
+    private AccountRepository accountRepository;
+
+    @Autowired
+    private WithdrawRepository withdrawRepository;
+
+    @Autowired
+    private TransferRepository transferRepository;
+
+    @Test
+    public void testGetTransactionInTenLastTransaction() {
+        when(accountRepository.findByAccountNumber("111111")).thenReturn(new Account("11111", "Andi", 1000, "111111"));
+
+        Account account = accountRepository.findByAccountNumber("111111");
+        Transfer transfer1 = new Transfer(45, account, "333333");
+        Withdraw withdraw2 = new Withdraw(10, account);
+        Withdraw withdraw3 = new Withdraw(15, account);
+        Transfer transfer4 = new Transfer(10, account, "222222");
+        Transfer transfer5 = new Transfer(15, account, "333333");
+        Withdraw withdraw6 = new Withdraw(20, account);
+        Withdraw withdraw7 = new Withdraw(25, account);
+        Transfer transfer8 = new Transfer(30, account, "222222");
+        Transfer transfer9 = new Transfer(35, account, "333333");
+        Transfer transfer10 = new Transfer(40, account, "222222");
+        Transfer transfer11 = new Transfer(45, account, "333333");
+        Transfer transfer12 = new Transfer(45, account, "333333");
+        Transfer savedTransfer1 = transferRepository.save(transfer1);
+        Withdraw savedWithdraw2 = withdrawRepository.save(withdraw2);
+        Withdraw savedWithdraw3 = withdrawRepository.save(withdraw3);
+        Transfer savedTransfer4 = transferRepository.save(transfer4);
+        Transfer savedTransfer5 = transferRepository.save(transfer5);
+        Withdraw savedWithdraw6 = withdrawRepository.save(withdraw6);
+        Withdraw savedWithdraw7 = withdrawRepository.save(withdraw7);
+        Transfer savedTransfer8 = transferRepository.save(transfer8);
+        Transfer savedTransfer9 = transferRepository.save(transfer9);
+        Transfer savedTransfer10 = transferRepository.save(transfer10);
+        Transfer savedTransfer11 = transferRepository.save(transfer11);
+        Transfer savedTransfer12 = transferRepository.save(transfer12);
+
+        List<Transaction> lastTransactions = transactionRepository.findLastTransaction(10);
+
+        assertThat(lastTransactions, hasSize(10));
+        assertThat(lastTransactions, hasItem(savedTransfer12));
+        assertThat(lastTransactions, not(hasItem(withdraw2)));
+    }
+}
