fea44df7374dd074eb61ac0b0b6df8f30d852118
Charel Samuel Matulessy <Charel_SM111@mitrais.com>
2020-03-05 15:39:06 +0700

    use repository instead of entitymanager

--- src/test/java/com/app/atmsimulation/AccountRepositoryTest.java
+++ src/test/java/com/app/atmsimulation/AccountRepositoryTest.java
@@ -2,12 +2,10 @@ package com.app.atmsimulation;
 
 import com.app.atmsimulation.model.Account;
 import com.app.atmsimulation.repository.AccountRepository;
-import org.hamcrest.CoreMatchers;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import java.util.List;
@@ -19,16 +17,13 @@ import static org.junit.Assert.*;
 public class AccountRepositoryTest {
 
     @Autowired
-    private TestEntityManager entityManager;
-
-    @Autowired
     private AccountRepository accountRepository;
 
     @Test
     public void testFindByAccountNumber() {
         Account account = new Account("112233", "Charel Samuel", 100, "123456");
 
-        entityManager.persist(account);
+        accountRepository.save(account);
 
         Account newAccount = accountRepository.findByAccountNumber("112233");
         assertEquals("112233", newAccount.getAccountNumber());
@@ -41,7 +36,7 @@ public class AccountRepositoryTest {
     public void testFindByAccountNumberAndPin() {
         Account account = new Account("112233", "Charel Samuel", 100, "123456");
 
-        entityManager.persist(account);
+        accountRepository.save(account);
 
         Account newAccount = accountRepository.findByAccountNumberAndPin("112233", "123456");
         assertEquals("112233", newAccount.getAccountNumber());
@@ -54,8 +49,8 @@ public class AccountRepositoryTest {
     public void testFindAll() {
         Account account1 = new Account("112233", "Charel Samuel", 100, "123456");
         Account account2 = new Account("112244", "Jonathan", 200, "111111");
-        entityManager.persist(account1);
-        entityManager.persist(account2);
+        accountRepository.save(account1);
+        accountRepository.save(account2);
 
         List<Account> accounts = accountRepository.findAll();
         assertEquals(2, accounts.size());
