4a8403877eaa38a1d03f86a6aa90361ffd1c8330
Charel Samuel Matulessy <Charel_SM111@mitrais.com>
2020-03-05 14:14:16 +0700

    add message properties

--- src/main/java/com/app/atmsimulation/Main.java
+++ src/main/java/com/app/atmsimulation/Main.java
@@ -2,12 +2,22 @@ package com.app.atmsimulation;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.MessageSource;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.support.ResourceBundleMessageSource;
 
 @SpringBootApplication
 public class Main {
 
     public static void main(String[] args) {
         SpringApplication.run(Main.class, args);
+    }
+
+    @Bean
+    public MessageSource messageSource () {
+        ResourceBundleMessageSource messageSource = new ResourceBundleMessageSource();
+        messageSource.setBasename("message");
+        return messageSource;
     }
 
 }
