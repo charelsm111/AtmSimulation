5e07d6479c93d5947a59f2387d0fdb87fbcaa6a6
Charel Samuel Matulessy <Charel_SM111@mitrais.com>
2020-03-06 14:04:28 +0700

    add account_id parameter for find transaction

--- src/main/java/com/app/atmsimulation/repository/TransactionRepository.java
+++ src/main/java/com/app/atmsimulation/repository/TransactionRepository.java
@@ -10,5 +10,5 @@ import java.util.List;
 @Repository
 public interface TransactionRepository extends JpaRepository<Transaction, Long> {
 
-    List<Transaction> findByDateOrderByIdDesc(LocalDate date);
+    List<Transaction> findByDateAndAccountIdOrderByIdDesc(LocalDate date, Long accountId);
 }
