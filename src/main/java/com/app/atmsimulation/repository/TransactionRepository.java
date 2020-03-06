552a1b9b2416efbcbb0f08407baf90926c699494
Charel Samuel Matulessy <Charel_SM111@mitrais.com>
2020-03-06 08:46:55 +0700

    add findByDate query

--- src/main/java/com/app/atmsimulation/repository/TransactionRepository.java
+++ src/main/java/com/app/atmsimulation/repository/TransactionRepository.java
@@ -5,6 +5,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDate;
 import java.util.List;
 
 @Repository
@@ -12,4 +13,6 @@ public interface TransactionRepository extends JpaRepository<Transaction, Long> 
 
     @Query(value = "SELECT * FROM transaction ORDER BY id DESC LIMIT ?1", nativeQuery = true)
     List<Transaction> findLastTransaction(Integer amount);
+
+    List<Transaction> findByDate(LocalDate date);
 }
