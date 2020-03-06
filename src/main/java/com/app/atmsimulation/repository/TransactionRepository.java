06439f78173e089ab46e24686132041818e9be95
Charel Samuel Matulessy <Charel_SM111@mitrais.com>
2020-03-06 09:32:56 +0700

    remove findLastTenTransaction and add findByDateOrderByIdDesc

--- src/main/java/com/app/atmsimulation/repository/TransactionRepository.java
+++ src/main/java/com/app/atmsimulation/repository/TransactionRepository.java
@@ -2,7 +2,6 @@ package com.app.atmsimulation.repository;
 
 import com.app.atmsimulation.model.Transaction;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.time.LocalDate;
@@ -11,8 +10,5 @@ import java.util.List;
 @Repository
 public interface TransactionRepository extends JpaRepository<Transaction, Long> {
 
-    @Query(value = "SELECT * FROM transaction ORDER BY id DESC LIMIT ?1", nativeQuery = true)
-    List<Transaction> findLastTransaction(Integer amount);
-
-    List<Transaction> findByDate(LocalDate date);
+    List<Transaction> findByDateOrderByIdDesc(LocalDate date);
 }
