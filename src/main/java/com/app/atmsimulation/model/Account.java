0711cc001a1a53bae0ac027d0d1b52fdb4d4fef0
Charel Samuel Matulessy <Charel_SM111@mitrais.com>
2020-03-09 09:41:04 +0700

    change id to identity type

--- src/main/java/com/app/atmsimulation/model/Account.java
+++ src/main/java/com/app/atmsimulation/model/Account.java
@@ -7,7 +7,7 @@ import java.util.List;
 public class Account {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     @Column
