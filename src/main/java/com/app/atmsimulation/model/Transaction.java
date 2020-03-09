da958a6d6d18956c3d7f2e91648d9e7f3d666383
Charel Samuel Matulessy <Charel_SM111@mitrais.com>
2020-03-09 09:57:36 +0700

    change id generation type to IDENTITY for auto_increment

--- src/main/java/com/app/atmsimulation/model/Transaction.java
+++ src/main/java/com/app/atmsimulation/model/Transaction.java
@@ -12,7 +12,7 @@ import static javax.persistence.CascadeType.MERGE;
 public class Transaction {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     @Column
